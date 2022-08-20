34eadd457bb7c523314995a3f152bbe140823fb3
jacob <jacob@kineviz.com>
2022-08-20 19:44:50 +0800

    modify build

--- build.sh
+++ build.sh
@@ -1,39 +1,29 @@
-::cmd 
-::deploy.bat 
-@echo off 
-hugo 
-:: public 
-echo. 
-echo cd to public... 
-cd public 
-echo. 
-echo add all files... 
-git add . 
-echo. 
-echo print status of Git Submodle... 
-git status 
-echo. 
-echo commit files... 
-git commit -m "update blog" 
-echo. 
-echo push Submodle to GitHub... 
-git push 
-:: father repo 
-echo. 
-echo back to repo... 
-cd .. 
-echo. 
-echo add all files... 
-git add . 
-echo. 
-echo print status of Git repo... 
-git status 
-echo. 
-echo commit files... 
-git commit -m "update blog" 
-echo. 
-echo push repo to GitHub... 
-git push 
-:: finsh 
-echo. 
-echo deploy finished. 
+#!/bin/bash
+# Program:
+#   发布Hugo生成文件到GitHub Pages
+# History:
+# 2020.05.05 GeekWho First release.
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Build the project.
+hugo
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="Published on $(date +'%Y-%m-%d %H:%M:%S')"
+if [ -n "$*" ]; then
+    msg="$*"
+fi
+git commit -m "$msg"
+
+git pull --rebase origin master
+# Push source and build repos.
+git push -f origin master
