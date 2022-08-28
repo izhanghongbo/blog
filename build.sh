86aed257e6b33e8977a1dfc77584d795515d7a91
jacob <jacob@kineviz.com>
2022-08-29 01:14:39 +0800

    Published on 2022-08-29 01:14:38

--- build.sh
+++ build.sh
@@ -8,6 +8,21 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="Published on $(date +'%Y-%m-%d %H:%M:%S')"
+if [ -n "$*" ]; then
+    msg="$*"
+fi
+git commit -m "$msg"
+
+git pull --rebase origin master
+# Push source and build repos.
+git push -f origin master
+
+
 # Build the project.
 hugo
 
